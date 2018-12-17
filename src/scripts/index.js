4ac2af10363a81f14a5689601b32f3b89a488558
Aleksey Denisov <alde.owl@gmail.com>
2018-12-17 14:04:07 +0700

    -

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,8 @@
-var feed = new Instafeed({
-    get: 'user',
-    userId: '212406086',
-    accessToken:'212406086.b242790.ad724d1a02ee43ca812e5ace3850f277',
-    // template: '12'
-});
-feed.run();
+(function (){
+    let feed = new Instafeed({
+        get: 'user',
+        userId: '212406086',
+        accessToken:'212406086.b242790.ad724d1a02ee43ca812e5ace3850f277',
+    });
+    feed.run();
+})();
